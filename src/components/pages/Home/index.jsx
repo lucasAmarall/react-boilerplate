298387e907e6227a8bb864d5c28bb4903788818c
Lucas <lucas.fickel53@gmail.com>
2020-04-06 18:49:21 -0300

    remove title of home page

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container, Img } from "./style";
 import Logo from "../../../assets/images/logo.svg";
-import Title from "../../atoms/Title";
 
 const Home = () => {
 	return(
 		<Container>
 			<Img src={Logo} alt="logo" />
-			<Title />
 		</Container>
 	);
 };
