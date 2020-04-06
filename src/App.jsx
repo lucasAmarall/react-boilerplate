b6d903a2c7cbac417400dc730782aed35efd5c2a
Lucas <lucas.fickel53@gmail.com>
2020-04-06 03:01:26 -0300

    linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import * as colors from './constants/colors.js';
-import {  ThemeProvider } from 'styled-components';
-import Home from './components/pages/Home/index.jsx';
-import './sass/main.sass'
+import React from "react";
+import * as colors from "./constants/colors.js";
+import {  ThemeProvider } from "styled-components";
+import Home from "./components/pages/Home/index.jsx";
+import "./sass/main.sass";
 
-const styleSettings = {
-	colors: colors['Light']
+const App = () => {
+	const styleSettings = {
+		colors: colors["light"]
+	};
+
+	return(
+		<ThemeProvider theme={styleSettings}>
+			<>
+				<Home />
+			</>
+		</ThemeProvider>
+	);
 };
 
-const App = () => (
-	<ThemeProvider theme={styleSettings}>
-		<>
-			<Home />
-		</>
-	</ThemeProvider>
-);
-
-export default App
+export default App;
--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Container, Img } from './style';
-import Logo from '../../../assets/images/logo.svg';
+import React from "react";
+import { Container, Img } from "./style";
+import Logo from "../../../assets/images/logo.svg";
+import Title from "../../atoms/Title";
 
 const Home = () => {
 	return(
 		<Container>
 			<Img src={Logo} alt="logo" />
+			<Title />
 		</Container>
 	);
 };
 
-export default Home
+export default Home;
