837c5ae2e5f8f750ab91433f13325a708ca58689
Lucas <lucas.fickel53@gmail.com>
2020-04-06 20:11:52 -0300

    add linter and prettier config

--- src/api/index.js
+++ src/api/index.js
@@ -1,46 +1,46 @@
 import axios from "axios";
 
 class APIService {
-	get host(){
-		const HOSTS = {
-			local: "http://dev.com.br",
-			dev: "http://dev.com.br",
-			qa: "http://qa.com.br",
-			staging: "http://staging.com.br",
-			prod: "http://prod.com.br",
-		};
+  static get host(){
+    const HOSTS = {
+      local: "http://dev.com.br",
+      dev: "http://dev.com.br",
+      qa: "http://qa.com.br",
+      staging: "http://staging.com.br",
+      prod: "http://prod.com.br",
+    };
 
-		const host = window.location.hostname;
-		const env = Object.keys(HOSTS).find(_env => host.includes(_env));
-		return HOSTS[env] || HOSTS.prod;
-	}
+    const host = window.location.hostname;
+    const env = Object.keys(HOSTS).find(_env => host.includes(_env));
+    return HOSTS[env] || HOSTS.prod;
+  }
   
-	constructor(){
-		this.service = axios.create({
-			baseURL: APIService.host,
-			timeout: 30000,
-		});
-	}
+  constructor(){
+    this.service = axios.create({
+      baseURL: APIService.host,
+      timeout: 30000,
+    });
+  }
 
-	get(resource, params) {
-		return this.service.get(resource, params);
-	}
+  get(resource, params) {
+    return this.service.get(resource, params);
+  }
 
-	post(resource, params) {
-		return this.service.post(resource, params);
-	}
+  post(resource, params) {
+    return this.service.post(resource, params);
+  }
 
-	put(resource, params) {
-		return this.service.put(resource, params);
-	}
+  put(resource, params) {
+    return this.service.put(resource, params);
+  }
 
-	delete(resource) {
-		return this.service.delete(resource);
-	}
+  delete(resource) {
+    return this.service.delete(resource);
+  }
 
-	patch(resource, params) {
-		return this.service.patch(resource, params);
-	}
+  patch(resource, params) {
+    return this.service.patch(resource, params);
+  }
 
 }
 
--- src/components/atoms/Title/index.jsx
+++ src/components/atoms/Title/index.jsx
@@ -3,12 +3,20 @@ import PropTypes from "prop-types";
 
 import { H1 } from "./styles";
 
-const Title = ({size, children, onClick}) => <H1 onClick={() => onClick("Example")} size={size}>{children}</H1>;
-
-Title.propTypes = {
-	size: PropTypes.oneOf(["small", "meddium", "large"]),
-	children: PropTypes.node.isRequired,
-	onClick: PropTypes.func
+const Title = ({size, children, onClick}) => {
+  return(
+    <H1 onClick={() => onClick("Example")} size={size}>{children}</H1>
+  );
 };
 
+Title.propTypes = {
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func
+};
+
+Title.defaultProps = {
+  size: "medium",
+  onClick: () => {}
+};
 export default Title;
