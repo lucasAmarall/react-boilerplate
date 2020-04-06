52fac04f264dd60d23882fd300caa41d7686b777
Lucas <lucas.fickel53@gmail.com>
2020-04-06 03:03:49 -0300

    update pattern

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,10 +1,10 @@
-export const Light = {
+export const light = {
 	primary: "#000925e3",
 	white: "#fff",
 	black: "#000",
 };
 
-export const Dark = {
+export const dark = {
 	primary: "#000925e3",
 	white: "#fff",
 	black: "#000",
