d11a27909ec643eebf6ec1781d67caf2f8d1aa81
Lucas <lucas.fickel53@gmail.com>
2020-04-06 20:11:52 -0300

    add linter and prettier config

--- src/components/atoms/Title/styles.js
+++ src/components/atoms/Title/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 const sizes = {
-	"small": "1em",
-	"medium": "1.5em",
-	"large": "2em",
+  "small": "1em",
+  "medium": "1.5em",
+  "large": "2em",
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export const H1 = styled.h1`
   color: ${({theme}) => theme.colors.primaryText};
 	font-size: ${({size}) => sizes[size]}
