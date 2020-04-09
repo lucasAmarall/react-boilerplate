b6da9d42a04adb07569cfac3a055a6e9cfb4cf3f
Lucas <lucas.fickel53@gmail.com>
2020-04-09 19:16:20 -0300

    feat(i18n): add i18n plugin and one example

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Logo from "@assets/images/logo.svg";
+import { useTranslation } from "react-i18next";
+import Title from "@atoms/Title";
 import { Container, Img } from "./style";
 
 const Home = () => {
+  const { t } = useTranslation();
   return (
     <Container>
+      <Title>{t("example")}</Title>
       <Img src={Logo} alt="logo" />
     </Container>
   );
