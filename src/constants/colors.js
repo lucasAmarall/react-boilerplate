f9a497cbc3e3979e9440dde49e658da711e2886d
Lucas <lucas.fickel53@gmail.com>
2020-04-09 19:19:19 -0300

    fix(colors): simplify colors example

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,15 +1,9 @@
 export const light = {
   primary: "#000925e3",
-  primaryText: "#000",
-  white: "#fff",
-  black: "#000",
-  loadingBackground: "#000925e3"
-};
-
-export const dark = {
-  primary: "#000925e3",
   primaryText: "#fff",
   white: "#fff",
   black: "#000",
   loadingBackground: "#000925e3"
 };
+
+export const dark = light
